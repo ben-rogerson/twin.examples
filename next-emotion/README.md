8fd552bc3f405308b10afcd0ad35382022840915
Ben Rogerson <info@benrogerson.com.au>
2020-09-02 21:03:36 +0930

    add next-emotion

---
+++ next-emotion/README.md
@@ -0,0 +1,6 @@
+# Next + Emotion + Tailwind + Twin
+
+- [Next](https://nextjs.org/)
+- [Emotion](https://emotion.sh/docs/introduction)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Twin](https://github.com/ben-rogerson/twin.macro)
---
+++ next-emotion/next.config.js
@@ -0,0 +1,14 @@
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
+})
