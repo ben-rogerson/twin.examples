294480efe8f209be0ee3208632d9cfc47f54feb3
Ben Rogerson <info@benrogerson.com.au>
2020-09-02 20:26:11 +0930

    add gatsby-styled-components

---
+++ gatsby-styled-components/src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import tw from 'twin.macro'
+import { Button, Logo } from '../components'
+
+const App = () => (
+  <div
+    css={[
+      tw`flex flex-col items-center justify-center h-screen`,
+      tw`bg-gradient-to-b from-electric to-ribbon`
+    ]}
+  >
+    <div tw="flex flex-col justify-center h-full space-y-5">
+      <Button isPrimary>Submit</Button>
+      <Button isSecondary>Cancel</Button>
+      <Button isSmall>Close</Button>
+    </div>
+    <Logo />
+  </div>
+)
+
+export default App
