efcd2adbab699722611d6f10972e6fbf09033ec0
Ben Rogerson <info@benrogerson.com.au>
2020-09-04 07:09:32 +0930

    add snowpack-react-styled-components

---
+++ snowpack-react-styled-components/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import tw from 'twin.macro';
+import { Button, Logo } from './components';
+import 'tailwindcss/dist/base.min.css';
+
+export default () => (
+  <div
+    css={[
+      tw`flex flex-col items-center justify-center h-screen`,
+      tw`bg-gradient-to-b from-electric to-ribbon`,
+    ]}
+  >
+    <div tw="flex flex-col justify-center h-full space-y-5">
+      <Button isPrimary>Submit</Button>
+      <Button isSecondary>Cancel</Button>
+      <Button isSmall>Close</Button>
+    </div>
+    <Logo />
+  </div>
+);
