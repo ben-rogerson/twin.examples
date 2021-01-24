bfdef14c3867a1f2f964a6ecce3e577021f5c274
Ben Rogerson <info@benrogerson.com.au>
2021-01-24 10:45:02 +1030

    Gatsby+sc: Update example and readme

--- gatsby-styled-components/src/components/Layout.js
+++ gatsby-styled-components/src/components/Layout.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import GlobalStyles from './GlobalStyles'
+import tw from 'twin.macro'
 
-const Layout = ({ children, ...rest }) => (
-  <div {...rest}>
+const Layout = ({ children }) => (
+  <div
+    css={[
+      tw`flex flex-col items-center justify-center h-screen
+      bg-gradient-to-b from-electric to-ribbon`,
+    ]}
+  >
     <GlobalStyles />
     {children}
   </div>
--- gatsby-styled-components/src/pages/index.js
+++ gatsby-styled-components/src/pages/index.js
@@ -3,12 +3,7 @@ import tw from 'twin.macro'
 import { Button, Logo, Layout } from '../components'
 
 const App = () => (
-  <Layout
-    css={[
-      tw`flex flex-col items-center justify-center h-screen`,
-      tw`bg-gradient-to-b from-electric to-ribbon`,
-    ]}
-  >
+  <Layout>
     <div tw="flex flex-col justify-center h-full space-y-5">
       <Button isPrimary>Submit</Button>
       <Button isSecondary>Cancel</Button>
