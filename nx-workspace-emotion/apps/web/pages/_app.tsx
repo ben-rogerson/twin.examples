3761008907fae0bf986951ae85d9d7f71d1f4157
Michele Memoli <michele@100shapes.com>
2021-02-17 08:51:40 +0700

    Renamed shared-ui Project

--- nx-workspace-emotion/apps/web/pages/_app.tsx
+++ nx-workspace-emotion/apps/web/pages/_app.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
--- nx-workspace-emotion/libs/shared-ui/src/lib/Button/Button.tsx
+++ nx-workspace-emotion/libs/shared-ui/src/lib/Button/Button.tsx
@@ -6,32 +6,34 @@ type types = {
   isSmall?: boolean;
 };
 
-const Button = styled.button(({ isPrimary, isSecondary, isSmall }: types) => [
-  // The common button styles added with the tw import
-  tw`text-lg px-8 py-2 rounded focus:outline-none`,
-  tw`transform transition-transform duration-75`,
+export const Button = styled.button(
+  ({ isPrimary, isSecondary, isSmall }: types) => [
+    // The common button styles added with the tw import
+    tw`text-lg px-8 py-2 rounded focus:outline-none`,
+    tw`transform transition-transform duration-75`,
 
-  // Use the variant grouping feature to add variants to multiple classes
-  tw`hocus:(scale-105 text-yellow-400)`,
+    // Use the variant grouping feature to add variants to multiple classes
+    tw`hocus:(scale-105 text-yellow-400)`,
 
-  // Use props to conditionally style your components
-  isPrimary && tw`bg-black text-white border-black`,
+    // Use props to conditionally style your components
+    isPrimary && tw`bg-black text-white border-black`,
 
-  // Combine regular css with tailwind classes within backticks
-  isSecondary && [
+    // Combine regular css with tailwind classes within backticks
+    isSecondary && [
+      css`
+        box-shadow: 0 0.1em 0 0 rgba(0, 0, 0, 0.25);
+      `,
+      tw`border-2 border-yellow-600`,
+    ],
+
+    // Conditional props can be added
+    isSmall ? tw`text-sm` : tw`text-lg`,
+
+    // The theme import can supply values from your tailwind.config.js
     css`
-      box-shadow: 0 0.1em 0 0 rgba(0, 0, 0, 0.25);
+      color: ${theme`colors.white`};
     `,
-    tw`border-2 border-yellow-600`,
-  ],
-
-  // Conditional props can be added
-  isSmall ? tw`text-sm` : tw`text-lg`,
-
-  // The theme import can supply values from your tailwind.config.js
-  css`
-    color: ${theme`colors.white`};
-  `,
-]);
+  ]
+);
 
 export default Button;
