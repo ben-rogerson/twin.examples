719dc18b228d5b33522de7a7856480c960c7df26
Ben Rogerson <info@benrogerson.com.au>
2021-03-03 20:33:13 +1030

    Convert comment types to avoid codesandbox error

--- gatsby-styled-components/src/components/GlobalStyles.js
+++ gatsby-styled-components/src/components/GlobalStyles.js
@@ -1,8 +1,6 @@
-/**
- * Global styles get added in the wrong order when using styled-components.
- * Exporting the styles from this file is the current workaround to this issue.
- * https://github.com/ben-rogerson/twin.macro/issues/277#issuecomment-754240911
- */
+// Global styles get added in the wrong order when using styled-components.
+// Exporting the styles from this file is the current workaround to this issue.
+// https://github.com/ben-rogerson/twin.macro/issues/277#issuecomment-754240911
 
 import React from 'react'
 import { GlobalStyles } from 'twin.macro'
