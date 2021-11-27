c1d8dfe2ad3e11919b6357972f53b8aa852056a5
Ben Rogerson <info@benrogerson.com.au>
2021-11-27 10:35:41 +1030

    Next-emotion-ts: Fix GlobalStyles import

--- next-emotion-typescript/pages/_app.tsx
+++ next-emotion-typescript/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import { GlobalStyles } from 'twin.macro'
+import GlobalStyles from './../styles/GlobalStyles'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <div>
