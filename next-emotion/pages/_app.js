b40b95b250c934082ea673f8cb013dd549ccee19
jqkk <kkj2551@naver.com>
2023-02-15 12:47:02 +0900

    fix: remove unnecessary code&lib in next-emotion

--- next-emotion/pages/_app.js
+++ next-emotion/pages/_app.js
@@ -1,12 +1,10 @@
-import { cache } from '@emotion/css'
-import { CacheProvider } from '@emotion/react'
-import GlobalStyles from './../styles/GlobalStyles'
+import GlobalStyles from '../styles/GlobalStyles'
 
 const App = ({ Component, pageProps }) => (
-  <CacheProvider value={cache}>
+  <>
     <GlobalStyles />
     <Component {...pageProps} />
-  </CacheProvider>
+  </>
 )
 
 export default App
--- next-emotion/pages/_document.js
+++ next-emotion/pages/_document.js
@@ -1,34 +1,16 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { extractCritical } from '@emotion/server'
+import { Head, Html, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    const critical = extractCritical(initialProps.html)
-    initialProps.html = critical.html
-    initialProps.styles = (
-      <React.Fragment>
-        {initialProps.styles}
-        <style
-          data-emotion-css={critical.ids.join(' ')}
-          dangerouslySetInnerHTML={{ __html: critical.css }}
-        />
-      </React.Fragment>
-    )
-
-    return initialProps
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
+
+export default Document
