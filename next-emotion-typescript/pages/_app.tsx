f1e38c946522c5692d02c80345b1d9521765399f
jqkk <kkj2551@naver.com>
2023-02-15 12:48:35 +0900

    fix: remove unnecessary code&lib in next-emotion-ts

--- next-emotion-typescript/pages/_app.tsx
+++ next-emotion-typescript/pages/_app.tsx
@@ -1,13 +1,11 @@
-import { cache } from '@emotion/css'
-import { CacheProvider } from '@emotion/react'
 import type { AppProps } from 'next/app'
-import GlobalStyles from './../styles/GlobalStyles'
+import GlobalStyles from '../styles/GlobalStyles'
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <CacheProvider value={cache}>
+  <>
     <GlobalStyles />
     <Component {...pageProps} />
-  </CacheProvider>
+  </>
 )
 
 export default App
--- next-emotion-typescript/pages/_document.tsx
+++ next-emotion-typescript/pages/_document.tsx
@@ -1,52 +1,16 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps
-} from 'next/document'
-import { extractCritical } from '@emotion/server'
+import React from 'react'
+import { Head, Html, Main, NextScript } from 'next/document'
 
-type NewDocumentInitialProps = DocumentInitialProps & {
-  ids: string[]
-  css: string
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-class CustomDocument extends Document<NewDocumentInitialProps> {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-    const critical = extractCritical(initialProps.html)
-    initialProps.html = critical.html
-    initialProps.styles = (
-      <>
-        {initialProps.styles}
-        <style
-          data-emotion-css={critical.ids.join(' ')}
-          dangerouslySetInnerHTML={{ __html: critical.css }}
-        />
-      </>
-    )
-
-    return initialProps
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <style
-            data-emotion-css={this.props?.ids?.join(' ')}
-            dangerouslySetInnerHTML={{ __html: this.props.css }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
-}
-
-export default CustomDocument
+export default Document
